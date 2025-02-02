5666073fa3b35ce49bfb3ddc4cfc8cd5a7261699
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-02 16:21:38 +0530

    add

---
+++ Fundamental Assignment 01 IICT/ConvertToGrames.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ConvertToGrames{
+
+	public static void main(String[] args){
+		
+		double ounces;
+		
+		System.out.print("Enter Input : ");
+		ounces = new Scanner(System.in).nextInt();
+		
+		System.out.println("Converted Value in Grames :"+(ounces*28.3495));
+		
+	}
+}
