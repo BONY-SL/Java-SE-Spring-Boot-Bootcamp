f6c599519905ca40aa75f4f23ce92213fb9f1aa3
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-02 16:32:20 +0530

    add

---
+++ Fundamental Assignment 01 IICT/SumAndAvg.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class SumAndAvg{
+
+	public static void main(String[] args){
+		
+		int[] array = {50,25,85,97,75,84,62,78,92};
+		double sum = 0;
+		double avg = 0;
+		
+		for(int i = 0 ; i < array.length ; i++){
+			
+			sum = sum + array[i];
+		}
+		
+		avg = sum/(array.length);
+		
+		System.out.println("Total : "+sum);
+		System.out.println("Average : "+avg);
+		
+		System.out.println(4+3+2);
+		System.out.println("4"+"3"+"2");
+		System.out.println('4'+'3'+'2');
+		System.out.println('4'+" "+'3'+" "+'2');
+		System.out.println('A'+'B'+'C');
+		System.out.println("A"+"B"+"C");
+		System.out.println('A'+100+200);
+		System.out.println('A'+" "+'B'+" "+'C');
+
+
+		int x=100,y;
+		y=++x;
+		System.out.println(x+" "+y);
+		y=++x;
+		System.out.println(x+" "+y);
+		y=++x;
+		System.out.println(x+" "+y);
+		
+		int x2=100,y2;
+		y2=x2++;
+		System.out.println(x2+" "+y2);
+		y2=x2++;
+		System.out.println(x2+" "+y2);
+		y2=x2++;
+		System.out.println(x2+" "+y2);
+		
+	}
+}
