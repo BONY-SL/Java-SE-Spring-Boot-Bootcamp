4ecbe0e9efecbc239912f3b18fd09a63c489a219
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-03 20:50:57 +0530

    Employee Salary

---
+++ Demo/ArraysSum.java
@@ -0,0 +1,26 @@
+public class ArraysSum {
+
+      static int sum = 0;
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {10,20,30,40,50,60};
+        int[] arr2 = {1,2,3,4,5};
+
+        ArraysSum arraysSum = new ArraysSum();
+
+        sum = arraysSum.sumOfArraysValues(arr1);
+        sum = arraysSum.sumOfArraysValues(arr2);
+
+        System.out.println(sum);
+
+    }
+
+    public int sumOfArraysValues(int[] array){
+
+        for (int j : array) {
+            sum = sum + j;
+        }
+        return sum;
+    }
+}
