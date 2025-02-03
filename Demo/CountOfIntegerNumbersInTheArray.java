6ad22e610c84d31b318487e57772d710cbd7bce5
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-03 21:26:14 +0530

    add

---
+++ Demo/CountOfIntegerNumbersInTheArray.java
@@ -0,0 +1,29 @@
+public class CountOfIntegerNumbersInTheArray {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {10,20,30,40,53,64};
+        int[] arr2 = {1,2,3,4,5};
+
+        int arrayOneIntegerCount;
+        int arrayTwoIntegerCount;
+
+        CountOfIntegerNumbersInTheArray array = new CountOfIntegerNumbersInTheArray();
+
+        arrayOneIntegerCount = array.numberOfIntegers(arr1);
+        arrayTwoIntegerCount = array.numberOfIntegers(arr2);
+
+        System.out.println(arrayOneIntegerCount);
+        System.out.println(arrayTwoIntegerCount);
+    }
+
+        public int numberOfIntegers(int[] array) {
+
+            int count = 0;
+            for (int j : array) {
+                if (j % 2 == 0)
+                    count++;
+            }
+            return count;
+        }
+}
