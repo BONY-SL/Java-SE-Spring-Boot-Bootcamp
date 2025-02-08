a3b87a8290b23ed071d1c575ce4d199f7de036d1
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-08 22:40:50 +0530

    add

---
+++ Demo/DemoCar.java
@@ -0,0 +1,64 @@
+public class DemoCar {
+    public static void main(String[] args) {
+
+        CarDetails car01 = new CarDetails();
+    }
+}
+
+class CarDetails{
+
+    private Integer id;
+    private String brand;
+    private String model;
+    private String transmission;
+    private String color;
+    private String year;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
