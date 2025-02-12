c5aa5439c163a09fa4e7382a78b0692d314bfb39
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-12 22:17:44 +0530

    add

---
+++ Demo/day08/StudentDemo.java
@@ -0,0 +1,17 @@
+package day08;
+
+import java.util.ArrayList;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+
+        String c = "kamal";
+        String d = "kamal";
+
+        String kamal = new String("kamal");
+        System.out.println(c==d);
+
+    }
+}
