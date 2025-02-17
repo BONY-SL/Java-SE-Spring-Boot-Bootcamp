7c65ba59e7cf260392c7a96010643e0422c650eb
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-02-17 21:36:14 +0530

    Instance Block , Static Block

---
+++ javaSpringBatch/src/main/java/demo/car/Car.java
@@ -0,0 +1,82 @@
+package demo.car;
+
+public class Car {
+
+    private  Integer registerdID;
+    private  String model;
+    private String color;
+    private String manufactureYear;
+    private String brand;
+
+    {
+        System.out.println("Instance Block Run");
+        // instance block provide default value for class instance variables
+    }
+
+    static {
+        System.out.println("Static Block Run");
+        // Run First
+        //static block provide default value for class variables
+    }
+    public Car(){
+
+    }
+
+    public Car(Integer registerdID, String model, String color, String manufactureYear, String brand) {
+        this.registerdID = registerdID;
+        this.model = model;
+        this.color = color;
+        this.manufactureYear = manufactureYear;
+        this.brand = brand;
+    }
+
+    public Integer getRegisterdID() {
+        return registerdID;
+    }
+
+    public void setRegisterdID(Integer registerdID) {
+        this.registerdID = registerdID;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(String manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString(){
+        return "Car ID : "+this.getRegisterdID() + "\n"+
+                "Car Model : "+this.getModel()+ "\n"+
+                "Color : "+this.getColor()+ "\n"+
+                "Manufacture Year : "+this.getManufactureYear()+ "\n"+
+                "Brand : "+this.getBrand()+ "\n";
+    }
+
+}
---
+++ javaSpringBatch/src/main/java/demo/car/DemoCar.java
@@ -0,0 +1,11 @@
+package demo.car;
+
+public class DemoCar {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        System.out.println(car1);
+
+    }
+}
